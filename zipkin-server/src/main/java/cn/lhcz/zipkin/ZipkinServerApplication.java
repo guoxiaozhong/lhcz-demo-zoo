c3a95177c40a217071c0f0eefe7d71b236858c63
guoxiaozhong <326167575@qq.com>
2021-04-16 17:41:58 +0800

    upload

---
+++ zipkin-server/src/main/java/cn/lhcz/zipkin/ZipkinServerApplication.java
@@ -0,0 +1,16 @@
+package cn.lhcz.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@EnableZipkinServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZipkinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
+}
